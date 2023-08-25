1a2f6006acf269e5342ea7ef5e59f5dd0c0748f6
v0es <markkikov@gmail.com>
2023-08-26 01:38:18 +0300

    added class for excel export

--- telegram_bot/utils/export/export.py
+++ telegram_bot/utils/export/export.py
@@ -11,7 +11,7 @@ class DBExport:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
         self.filename = self._generate_name()
-        self.filepath = rf'telegram_bot\utils\export\export_{self.filename}.xlsx'
+        self.filepath = rf'telegram_bot\utils\export\export_{self.filename}'
 
     def __del__(self):
         res = self._delete_file(self.filepath)
@@ -20,7 +20,7 @@ class DBExport:
 
     @staticmethod
     def _generate_name() -> str:
-        return f'{datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}'
+        return f'{datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.xlsx'
 
     @staticmethod
     def _delete_file(filepath: str):
@@ -34,22 +34,32 @@ class DBExport:
         workbook = xlsxwriter.Workbook(self.filepath)
         worksheet = workbook.add_worksheet()
 
-        date_format = workbook.add_format({'num_format': 'd mmmm yyyy HH:MM:SS'})  # add date format
+        date_format = workbook.add_format({  # add date format
+            'num_format': 'dd.mm.yy HH:MM:SS',
+            'align': 'center',
+            'valign': 'vcenter'
+            })
+
+        common_format = workbook.add_format({  # add aligning format
+            'align': 'center',
+            'valign': 'vcenter',
+        })
         
         user_list = await get_all_users(self.session)
         clients = list(filter(lambda user: not (user.is_admin or user.is_owner), user_list))
         export_cols = ['id', 'timestamp', 'telegram_id', 'fullname', 'phone_number', 'email', 'bonus_points']
         for index, col in enumerate(export_cols):
-            worksheet.write(0, index, col)
+            worksheet.write(0, index, col, common_format)
         for index, client in enumerate(clients):
-            worksheet.write(index + 1, 0, index + 1)
+            worksheet.write(index + 1, 0, index + 1, common_format)
             worksheet.write_datetime(index + 1, 1, client.timestamp, date_format)
-            worksheet.write(index + 1, 2, client.user_id)
-            worksheet.write(index + 1, 3, client.fullname)
-            worksheet.write(index + 1, 4, client.phone_number)
-            worksheet.write(index + 1, 5, client.email)
-            worksheet.write(index + 1, 6, client.bonus_points)
+            worksheet.write(index + 1, 2, client.user_id, common_format)
+            worksheet.write(index + 1, 3, client.fullname, common_format)
+            worksheet.write(index + 1, 4, client.phone_number, common_format)
+            worksheet.write(index + 1, 5, client.email, common_format)
+            worksheet.write(index + 1, 6, client.bonus_points, common_format)
         
+        worksheet.autofit()
         workbook.close()
         export_table = FSInputFile(self.filepath, self.filename)
         return export_table
