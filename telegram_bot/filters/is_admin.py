6a55cce36d49a098c2143f943f8419508c9bc491
v0es <markkikov@gmail.com>
2023-08-25 03:08:31 +0300

    added admin filter

--- telegram_bot/filters/is_admin.py
+++ telegram_bot/filters/is_admin.py
@@ -9,17 +9,23 @@ from exceptions import UserNotFoundException
 
 
 class IsAdmin(Filter):
+
     def __init__(self, session_pool: sessionmaker) -> None:
         self.session_pool = session_pool
 
     async def __call__(self, event: Message | CallbackQuery) -> bool:
-
+        # return True
         async with self.session_pool() as session:
             try:
                 if isinstance(event, Message):
-                    return await is_admin(session, event.from_user.id)
-                else:
-                    return await is_admin(session, event.message.from_user.id)
 
+                    admin_flag = await is_admin(session, event.from_user.id)
+                    return True if admin_flag else False
+
+                else:
+
+                    admin_flag = await is_admin(session, event.from_user.id)
+                    return True if admin_flag else False
+                
             except UserNotFoundException:
                 return False
