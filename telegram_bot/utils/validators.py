6f51816ab9397f6f482e57390f1bb273e46c45c6
v0es <markkikov@gmail.com>
2023-08-25 03:01:15 +0300

    added new validators

---
+++ telegram_bot/utils/validators.py
@@ -0,0 +1,29 @@
+import re
+
+
+def validate_phone_number(phone_number: str) -> bool:
+    regexp = '^\+\d{1,3}\d{3}\d{7}$'
+    if len(re.findall(regexp, phone_number)) == 1:
+        return True
+    else:
+        return False
+
+
+def validate_email(email: str) -> bool:
+    regexp = '^[^@\s]+@[^@\s]+\.[^@\s]+'
+    if len(re.findall(regexp, email)) == 1:
+        return True
+    else:
+        return False
+
+
+def validate_otp_codes(valid_code: str, user_code: str) -> bool:
+    pass
+
+
+def validate_fullname(fullname: str) -> bool:
+    return True
+
+
+def validate_order_amount(amount: str) -> bool:
+    return amount.isdigit()
