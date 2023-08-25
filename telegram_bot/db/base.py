f891926678e93a16eddadb666eb17f585ea320b1
v0es <markkikov@gmail.com>
2023-08-25 03:01:35 +0300

    removed update to timestamp

--- telegram_bot/db/base.py
+++ telegram_bot/db/base.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import as_declarative, mapped_column, Mapped, declared_attr
 @as_declarative()
 class BaseModel:
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
-    timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=False, server_default=current_timestamp(), onupdate=current_timestamp())
+    timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=False, server_default=current_timestamp())
     
     __allow_unmapped__ = False
 
